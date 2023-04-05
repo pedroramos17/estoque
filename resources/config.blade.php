503fa6c8b3388acdf185a2dfc22e1081f0b7ac7b
pedroramos17 <pedro_ramos2003@hotmail.com>
2023-04-05 17:18:33 -0300

    creating template and wip config page

---
+++ resources/config.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.template')
+@section('content')
+<div class="mx-4 space-y-4">
+  <div class="flex justify-between  mt-[63px]">
+    <h1 class="text-white font-regular text-3xl">Resultados da pesquisa</h1>
+  </div>
+</div>
---
+++ resources/views/layouts/template.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" href="favicon.svg" type="favicon.svg">
+
+        <title>Estoque</title>
+
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="bg-gray-900">
+        <header>
+          @include('./navbar')
+        </header>
+        @include('./sidebar')
+        
+        <main class="ml-[130px]">
+            @yield('content')
+        </main>
+    </body>
+</html>
+
+<script>
+  const menuButton = document.getElementById("menu-button");
+
+menuButton.addEventListener("click", function () {
+    let classList = document.getElementById("sidebar").classList;
+    classList.toggle("hidden");
+    classList.toggle("block");
+
+    let marginMain = document.getElementsByTagName("main")[0].classList;
+    marginMain.toggle("ml-[130px]");
+});
+
+</script>
